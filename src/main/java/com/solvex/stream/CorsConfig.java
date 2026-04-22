6954735ea1b550305e12e1aed30f3207373d5112
Sunil Kumar Gupta <62325742+Sunil-731820@users.noreply.github.com>
2026-04-22 12:13:04 +0530

    Update CorsConfig.java

--- src/main/java/com/solvex/stream/CorsConfig.java
+++ src/main/java/com/solvex/stream/CorsConfig.java
@@ -1,12 +1,13 @@
 package com.solvex.stream;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
