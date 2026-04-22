cbc5f449c08fd31c5efc64e4bc06c4cf63eb8234
sunil yadav <94454236+Sunil9640@users.noreply.github.com>
2026-04-22 17:05:57 +0530

    Update Application.java

--- src/main/java/com/solvex/stream/Application.java
+++ src/main/java/com/solvex/stream/Application.java
@@ -19,6 +19,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class Application extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(SolveXStreamApplication.class);
+        return builder.sources(Application.class);
     }
 }
